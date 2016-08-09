3351f9734b688728d146bb68d00cbc05f793c6d4
Parth Sehgal <parthsehgal94@yahoo.com>
2016-08-09 13:11:55 +0530

    Create cache_matrix.R

---
+++ cache_matrix.R
@@ -0,0 +1,35 @@
+## Here a special matrix is created using makeCacheMatrix 
+## whose inverse is obtained and cached for future use
+
+## Using <<- operator Im saving my special matrix in global env. 
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+
+}
+
+
+## The following function calculates the inverse of the special "matrix" created with the above function. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse function.
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+        
+}
